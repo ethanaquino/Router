8c22f7a2d34a26cb42e6725a84db04ed885b3689
Pivotal <eaq1499@gmail.com>
2019-05-02 16:38:44 -0400

    having trouble rendering a drop down box in users.

--- src/App/Users.js
+++ src/App/Users.js
@@ -1,8 +1,23 @@
 import {Link} from "react-router-dom";
-import React from "react";
+import React from 'react';
+import Selector from '../index'
+import Select from "react-select";
 
 function Users() {
-    return <h2>Users</h2>;
+    render => {
+        return (
+            <div>
+                <h2>Users</h2>
+                <Select
+                    value={selectedOption}
+                    onChange={this.handleChange}
+                    options={options}
+                />
+            </div>
+        );
+
+
+    }
 }
 
 export default Users;
--- src/index.js
+++ src/index.js
@@ -3,9 +3,39 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App/App';
 import * as serviceWorker from './serviceWorker';
+import Select from "react-select";
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
+const options = [
+    {value: 'Will Dingus', label: 'Dingus'},
+    {value: 'James Poophead', label: 'Poophead'},
+    {value: 'Marco The Departed', label: 'Departed'},
+    {value: 'Nate Edwards', label: 'Nedwards'}
+];
+
+class Selector extends React.Component {
+    state = {
+        selectedOption: null,
+    }
+    handleChange = (selectedOption) => {
+        this.setState({selectedOption});
+        console.log('Option selected:', selectedOption);
+    }
+    render() {
+        const { selectedOption } = this.state;
+
+        return (
+            <Select
+                value={selectedOption}
+                onChange={this.handleChange}
+                options={options}
+            />
+        );
+    }
+}
+
+export default Selector;
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
